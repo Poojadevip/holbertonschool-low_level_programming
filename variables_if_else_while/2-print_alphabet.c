a16ad076a6d159dfad10d85e4d84f431105be591
poojadevip <6892@holbertonstudents.com>
2023-06-23 07:47:23 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - prints the alphabet in lowercase, followed by a new line
+*
+*Return: 0
+*/
+
+
+int main(void)
+{
+	char c;
+
+	c = 'a;
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		C++;
+	}
+	putchar('\n');
+
+	return (0);
+}
