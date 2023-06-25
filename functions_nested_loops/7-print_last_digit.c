f7f7c72c3a51e1e1c9828297681aa8edae514391
poojadevip <6892@holbertonstudents.com>
2023-06-25 07:02:24 -0700

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *print_last_digit - prints the last digit
- *@d: input to be checked
+ *@n: input to be checked
  *parameter: accepts the input
  *Return: last digit of integer
  */
@@ -9,7 +9,7 @@
 int print_last_digit(int n)
 {
 	int l;
-	
+
 	l = n % 10;
 	if (l < 0)
 	{
